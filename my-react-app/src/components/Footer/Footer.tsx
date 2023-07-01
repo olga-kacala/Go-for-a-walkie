6a7ec1ca80f2156639eaabed2cf720349e9ee065
olga-kacala <olga.kacala@gmail.com>
2023-07-01 16:58:24 +0200

    Footer logos

--- my-react-app/src/components/Footer/Footer.tsx
+++ my-react-app/src/components/Footer/Footer.tsx
@@ -1,10 +1,22 @@
-import classes from './Footer.module.css';
+import classes from "./Footer.module.css";
 
-export function Footer () {
-
-    return (
-        <footer className={classes.footer}>
-        <p>&copy; 2023 Walkie. All rights reserved.</p>
-        </footer>
-    )
+export function Footer() {
+  return (
+    <footer className={classes.footer}>
+      <p>&copy; 2023 Walkie. All rights reserved.</p>
+      <div className={classes.logos}>
+        <p>Author:</p> 
+        <a href="https://github.com/olga-kacala" target="_blank">
+          <img title="My GH" alt="GitHub" src={"../../Img/GitHub.png"} />
+        </a>
+        <a href="https://www.linkedin.com/in/olga-kacala/" target="_blank">
+          <img
+            title="My LinkedIn"
+            alt="LinkedIn"
+            src={"../../Img/LinkedIn.png"}
+          />
+        </a>
+      </div>
+    </footer>
+  );
 }
