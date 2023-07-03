39348fd9599be28a2e26f9857fc635f7eb3cba7b
olga-kacala <olga.kacala@gmail.com>
2023-07-03 16:19:59 +0200

    Firebase set

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -5,6 +5,18 @@ import {Home} from './components/Home/Home';
 import { Footer } from './components/Footer/Footer';
 import { Login } from './components/Login/Login';
 import { Header } from './components/Header/Header';
+import { onAuthStateChanged } from "firebase/auth";
+import { getDoc, doc } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./firebase";
+
+const firebaseApp = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(firebaseApp);
+export const firebaseAuth = getAuth(firebaseApp);
+export const firebaseDb = getFirestore(firebaseApp)
+
 
 function App() {
   return (
--- my-react-app/src/components/Login/Login.tsx
+++ my-react-app/src/components/Login/Login.tsx
@@ -1,24 +1,52 @@
-import classes from './Login.module.css';
+import classes from "./Login.module.css";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { firebaseAuth } from "../../App";
 
 export function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const [isinputError, setIsInputError] = useState(false);
 
-    return (
-  <div>
-  <div className={classes.registration}>
+  const signIn = async (e: React.FormEvent): Promise<void> => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(firebaseAuth, username, password);
+      navigate("*");
+    } catch ({ message }) {
+      console.log(message);
+      setIsInputError(true);
+      setError("Sorry, wrong password or login. Pls try again");
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+    }
+  };
+
+  return (
+    <div>
+      <div className={classes.registration}>
         <h2>Create an Account</h2>
         <form>
           <input type="text" placeholder="Name" required />
           <input type="email" placeholder="Email" required />
           <input type="password" placeholder="Password" required />
-          <button type="submit" className={classes.button}>Sign Up</button>
+          <button type="submit" className={classes.button}>
+            Sign Up
+          </button>
         </form>
         <p>
-          Already have an account? <a href="login.html" className={classes.button}>Log in</a>
+          Already have an account?{" "}
+          <a href="login.html" className={classes.button}>
+            Log in
+          </a>
         </p>
       </div>
-  </div>
- 
-    )
+    </div>
+  );
 }
