693fc59961160b994d68dbe627c1a2631848948a
olga-kacala <olga.kacala@gmail.com>
2023-07-08 17:02:08 +0200

    Logout

--- my-react-app/src/components/Header/Header.tsx
+++ my-react-app/src/components/Header/Header.tsx
@@ -1,32 +1,42 @@
 import classes from "./Header.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import { AppContext } from "../Providers/Providers";
+import { signOut } from "firebase/auth";
+import { firebaseAuth } from "../../App";
 
-export function Header():JSX.Element {
+export function Header(): JSX.Element {
+  const { isLogged, setIsLogged, username, myPets } = useContext(AppContext);
+  console.log("Header username:", username);
 
-  const {isLogged, setIsLogged, username, myPets} = useContext(AppContext);
-console.log("Header username:",username);
+  const navigate = useNavigate();
+
+  const handleLogout = async (): Promise<void> => {
+    try {
+      await signOut(firebaseAuth);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLogged(false);
+    navigate("/Logout");
+  };
 
   return (
     <div>
-    {/* {!isLogged}
-    <header className={classes.header}>
-      <Link className={classes.link} to="*">
-        <h1>Walkie</h1>
-      </Link>
-      <p>Find a Dog Walking Buddy </p>
-    </header> */}
-
-    {isLogged}
+      {isLogged}
       <header className={classes.header}>
-      <Link className={classes.link} to="*">
-        <h1>Walkie</h1>
-      </Link>
-      <p>Find a Dog Walking Buddy </p>
-    <Link className={classes.link} to="/MyPets">My Pets</Link>
-    </header>
-    <span>Hello, {username}!</span>
-   </div> 
+        <Link className={classes.link} to="*">
+          <h1>Walkie</h1>
+        </Link>
+        <p>Find a Dog Walking Buddy </p>
+        <span>Hello, {username}!</span>
+        <Link className={classes.link} to="/MyPets">
+          My Pets
+        </Link>
+        <Link className={classes.link} to="/Logout">
+          Log out
+        </Link>
+      </header>
+    </div>
   );
 }
