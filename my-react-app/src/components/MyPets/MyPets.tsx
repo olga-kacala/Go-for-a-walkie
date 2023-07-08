0735eea6638aa82696fe33c16f5663df157bdea5
olga-kacala <olga.kacala@gmail.com>
2023-07-08 19:43:31 +0200

    pet list

--- my-react-app/src/components/MyPets/MyPets.tsx
+++ my-react-app/src/components/MyPets/MyPets.tsx
@@ -1,6 +1,7 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../Providers/Providers";
 import { AddPet } from "../Providers/Providers";
+import classes from './MyPets.module.css';
 
 type MyPetsProps = {
     myPets: AddPet[];
@@ -8,10 +9,34 @@ type MyPetsProps = {
 
 export function MyPets ({myPets}:MyPetsProps):JSX.Element {
     const {resultMyPets} = useContext(AppContext);
+    const [petName, setPetName] = useState<string>('');
+    const [error, setError] = useState<string>('');
+
+    const handleSubmitPet = async (e:React.FormEvent): Promise<void>=> {
+        e.preventDefault();
+        console.log(petName)
+    }
     return (
-        <div>
-        <h1>My Pets</h1>
+        <div className={classes.login}>
+        <h2>My Pets</h2>
         <h2>{resultMyPets}</h2>
+        <form>
+          <input
+            name="pet name"
+            type="pet name"
+            value={petName}
+            placeholder="Pet name"
+            required
+            onChange={(e) => {
+              setPetName(e.target.value);
+            }}
+          />
+          
+          <p>{error}</p>
+          <button className={classes.button} onClick={handleSubmitPet}>
+            Add Pet
+          </button>
+        </form>
         </div>
     )
 }
