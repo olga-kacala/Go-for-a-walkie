7fb149c25bfb06e96e426185ed3f419fb562602a
olga-kacala <olga.kacala@gmail.com>
2023-07-10 14:08:21 +0200

    Logout styles

--- my-react-app/src/components/Logout/Logout.tsx
+++ my-react-app/src/components/Logout/Logout.tsx
@@ -1,14 +1,29 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import classes from "./Logout.module.css";
 
 export function Logout(): JSX.Element {
- 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("*");
+    }, 3000);
+  }, []);
+
   return (
-    <div>
-      <h2>You have been logged out</h2>
-      <h3>If you want to go for a Walkie</h3>
-      <button>
-        <Link to="/Login">Log in</Link>
-      </button>
+    <div className={classes.logoutContainer}>
+      <div className={classes.logout}>
+        <h2>Thank you for a Walk!</h2>
+        <img
+          src="/Img/Perek.jpg"
+          alt="Bird picture"
+          className={classes.Perek}
+        />
+        <div className={classes.bottomH2}>
+          <h2>Come again soon!</h2>
+        </div>
+      </div>
     </div>
   );
 }
