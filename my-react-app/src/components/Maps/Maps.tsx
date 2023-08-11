d271c073ed26bd9b915f68ad004b9180e62efa0c
olga-kacala <olga.kacala@gmail.com>
2023-08-11 19:43:22 +0200

    Google maps

--- my-react-app/src/components/Maps/Maps.tsx
+++ my-react-app/src/components/Maps/Maps.tsx
@@ -65,29 +65,28 @@
 //   );
 // }
 
+
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import { useMemo } from "react";
 import classes from "./Maps.module.css";
 
 export const Maps = () => {
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
-      });
-      const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
-    
-      return (
-        <div className={classes.Map}>
-          {!isLoaded ? (
-            <h1>Loading...</h1>
-          ) : (
-            <GoogleMap
-            mapContainerClassName="map-container"
-              center={center}
-              zoom={10}
-            >
-              <Marker position={{ lat: 18.52043, lng: 73.856743 }} />
-            </GoogleMap>
-          )}
-        </div>
-      );
-    };
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  });
+  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+
+  return (
+    <div className={classes.Map}>
+      {!isLoaded ? (
+        <h1>Loading...</h1>
+      ) : (
+        <GoogleMap
+          mapContainerClassName={classes.mapContainer}
+          center={center}
+          zoom={10}
+        />
+      )}
+    </div>
+  );
+};
