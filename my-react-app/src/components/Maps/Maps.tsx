5a14336cc4cb13952ca7f84f0d1f0c0a7fb03f84
olga-kacala <olga.kacala@gmail.com>
2023-08-29 13:54:35 +0200

    distance

--- my-react-app/src/components/Maps/Maps.tsx
+++ my-react-app/src/components/Maps/Maps.tsx
@@ -1,9 +1,111 @@
-import {
-  GoogleMap,
-  Marker,
-  useLoadScript,
-  Polyline,
-} from "@react-google-maps/api";
+// import {
+//   GoogleMap,
+//   Marker,
+//   useLoadScript,
+//   Polyline,
+// } from "@react-google-maps/api";
+// import { useState, useEffect, useRef } from "react";
+// import classes from "./Maps.module.css";
+
+// export const Maps = () => {
+//   const { isLoaded } = useLoadScript({
+//     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY || "",
+//   });
+
+//   const [userLocation, setUserLocation] = useState<{
+//     lat: number;
+//     lng: number;
+//   } | null>(null);
+//   const [startingMarker, setStartingMarker] = useState<{
+//     lat: number;
+//     lng: number;
+//   } | null>(null);
+//   const [markers, setMarkers] = useState<
+//     { lat: number; lng: number; id: number }[]
+//   >([]);
+//   const markerIdCounter = useRef(0);
+
+//   useEffect(() => {
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(
+//         (position) => {
+//           setUserLocation({
+//             lat: position.coords.latitude,
+//             lng: position.coords.longitude,
+//           });
+//           setStartingMarker({
+//             lat: position.coords.latitude,
+//             lng: position.coords.longitude,
+//           });
+//         },
+//         (error) => {
+//           console.error("Error fetching user location:", error);
+//         }
+//       );
+//     }
+//   }, []);
+
+//   const handleMapClick = (event: google.maps.MapMouseEvent) => {
+//     if (event.latLng) {
+//       const { lat, lng } = event.latLng.toJSON();
+//       setMarkers((prevMarkers) => [
+//         ...prevMarkers,
+//         { lat, lng, id: markerIdCounter.current++ },
+//       ]);
+//     }
+//   };
+
+//   const handleMarkerClick = (markerId: number) => {
+//     setMarkers((prevMarkers) =>
+//       prevMarkers.filter((marker) => marker.id !== markerId)
+//     );
+//   };
+
+//   return (
+//     <div className={classes.Map}>
+//       {!isLoaded ? (
+//         <h1>Loading...</h1>
+//       ) : (
+//         <GoogleMap
+//           mapContainerClassName={classes.mapContainer}
+//           center={
+//             userLocation || { lat: 50.65696776753784, lng: 17.9230121375674 }
+//           }
+//           zoom={10}
+//           onClick={handleMapClick}
+//         >
+//           {userLocation && startingMarker && (
+//             <>
+//               {markers.map((marker) => (
+//                 <Marker
+//                   key={marker.id}
+//                   position={{ lat: marker.lat, lng: marker.lng }}
+//                   onClick={() => handleMarkerClick(marker.id)}
+//                 />
+//               ))}
+//               {markers.length >= 2 && (
+//                 <Polyline
+//                   path={markers.map((marker) => ({
+//                     lat: marker.lat,
+//                     lng: marker.lng,
+//                   }))}
+//                   options={{
+//                     strokeColor: "rgba(122,146,254,1)",
+//                     strokeOpacity: 1,
+//                     strokeWeight: 3,
+//                   }}
+//                 />
+//               )}
+//             </>
+//           )}
+//         </GoogleMap>
+//       )}
+//     </div>
+//   );
+// };
+
+
+import { GoogleMap, Marker, useLoadScript, Polyline } from "@react-google-maps/api";
 import { useState, useEffect, useRef } from "react";
 import classes from "./Maps.module.css";
 
@@ -23,6 +125,7 @@ export const Maps = () => {
   const [markers, setMarkers] = useState<
     { lat: number; lng: number; id: number }[]
   >([]);
+  const [totalDistance, setTotalDistance] = useState<number>(0);
   const markerIdCounter = useRef(0);
 
   useEffect(() => {
@@ -44,6 +147,66 @@ export const Maps = () => {
       );
     }
   }, []);
+
+  useEffect(() => {
+    let distance = 0;
+
+    for (let i = 1; i < markers.length; i++) {
+      const prevMarker = markers[i - 1];
+      const currMarker = markers[i];
+      distance += calculateDistance(
+        prevMarker.lat,
+        prevMarker.lng,
+        currMarker.lat,
+        currMarker.lng
+      );
+    }
+
+    setTotalDistance(distance);
+  }, [markers]);
+
+  const calculateDistance = (lat1, lng1, lat2, lng2) => {
+    const earthRadius = 6371; // Radius of the Earth in kilometers
+  
+    // Convert latitude and longitude from degrees to radians
+    const latRad1 = (lat1 * Math.PI) / 180;
+    const lngRad1 = (lng1 * Math.PI) / 180;
+    const latRad2 = (lat2 * Math.PI) / 180;
+    const lngRad2 = (lng2 * Math.PI) / 180;
+  
+    // Calculate the differences between latitudes and longitudes
+    const latDiff = latRad2 - latRad1;
+    const lngDiff = lngRad2 - lngRad1;
+  
+    // Calculate the Haversine formula
+    const a =
+      Math.sin(latDiff / 2) * Math.sin(latDiff / 2) +
+      Math.cos(latRad1) * Math.cos(latRad2) * Math.sin(lngDiff / 2) * Math.sin(lngDiff / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  
+    // Calculate the distance
+    const distance = earthRadius * c;
+    return distance;
+  };
+
+  useEffect(() => {
+    let distance = 0;
+  
+    for (let i = 1; i < markers.length; i++) {
+      const prevMarker = markers[i - 1];
+      const currMarker = markers[i];
+      distance += calculateDistance(
+        prevMarker.lat,
+        prevMarker.lng,
+        currMarker.lat,
+        currMarker.lng
+      );
+    }
+  
+    setTotalDistance(distance);
+    console.log(distance)
+  }, [markers]);
+  
 
   const handleMapClick = (event: google.maps.MapMouseEvent) => {
     if (event.latLng) {
@@ -95,6 +258,11 @@ export const Maps = () => {
                     strokeWeight: 3,
                   }}
                 />
+              )}
+              {totalDistance > 0 && (
+                <div>
+                  Total Distance: {totalDistance.toFixed(2)} km
+                </div>
               )}
             </>
           )}
