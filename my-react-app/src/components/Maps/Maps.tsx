33b9f6c9895bb7d08c1235c1579285b58874e012
olga-kacala <olga.kacala@gmail.com>
2023-08-30 14:23:01 +0200

    walk pick list

--- my-react-app/src/components/Maps/Maps.tsx
+++ my-react-app/src/components/Maps/Maps.tsx
@@ -1,118 +1,19 @@
-// import {
-//   GoogleMap,
-//   Marker,
-//   useLoadScript,
-//   Polyline,
-// } from "@react-google-maps/api";
-// import { useState, useEffect, useRef } from "react";
-// import classes from "./Maps.module.css";
-
-// export const Maps = () => {
-//   const { isLoaded } = useLoadScript({
-//     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY || "",
-//   });
-
-//   const [userLocation, setUserLocation] = useState<{
-//     lat: number;
-//     lng: number;
-//   } | null>(null);
-//   const [startingMarker, setStartingMarker] = useState<{
-//     lat: number;
-//     lng: number;
-//   } | null>(null);
-//   const [markers, setMarkers] = useState<
-//     { lat: number; lng: number; id: number }[]
-//   >([]);
-//   const markerIdCounter = useRef(0);
-
-//   useEffect(() => {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//           setUserLocation({
-//             lat: position.coords.latitude,
-//             lng: position.coords.longitude,
-//           });
-//           setStartingMarker({
-//             lat: position.coords.latitude,
-//             lng: position.coords.longitude,
-//           });
-//         },
-//         (error) => {
-//           console.error("Error fetching user location:", error);
-//         }
-//       );
-//     }
-//   }, []);
-
-//   const handleMapClick = (event: google.maps.MapMouseEvent) => {
-//     if (event.latLng) {
-//       const { lat, lng } = event.latLng.toJSON();
-//       setMarkers((prevMarkers) => [
-//         ...prevMarkers,
-//         { lat, lng, id: markerIdCounter.current++ },
-//       ]);
-//     }
-//   };
-
-//   const handleMarkerClick = (markerId: number) => {
-//     setMarkers((prevMarkers) =>
-//       prevMarkers.filter((marker) => marker.id !== markerId)
-//     );
-//   };
-
-//   return (
-//     <div className={classes.Map}>
-//       {!isLoaded ? (
-//         <h1>Loading...</h1>
-//       ) : (
-//         <GoogleMap
-//           mapContainerClassName={classes.mapContainer}
-//           center={
-//             userLocation || { lat: 50.65696776753784, lng: 17.9230121375674 }
-//           }
-//           zoom={10}
-//           onClick={handleMapClick}
-//         >
-//           {userLocation && startingMarker && (
-//             <>
-//               {markers.map((marker) => (
-//                 <Marker
-//                   key={marker.id}
-//                   position={{ lat: marker.lat, lng: marker.lng }}
-//                   onClick={() => handleMarkerClick(marker.id)}
-//                 />
-//               ))}
-//               {markers.length >= 2 && (
-//                 <Polyline
-//                   path={markers.map((marker) => ({
-//                     lat: marker.lat,
-//                     lng: marker.lng,
-//                   }))}
-//                   options={{
-//                     strokeColor: "rgba(122,146,254,1)",
-//                     strokeOpacity: 1,
-//                     strokeWeight: 3,
-//                   }}
-//                 />
-//               )}
-//             </>
-//           )}
-//         </GoogleMap>
-//       )}
-//     </div>
-//   );
-// };
-
-
-import { GoogleMap, Marker, useLoadScript, Polyline } from "@react-google-maps/api";
-import { useState, useEffect, useRef } from "react";
+import {
+  GoogleMap,
+  Marker,
+  useLoadScript,
+  Polyline,
+} from "@react-google-maps/api";
+import { useState, useEffect, useRef, useContext } from "react";
 import classes from "./Maps.module.css";
+import { AppContext } from "../Providers/Providers";
 
 export const Maps = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY || "",
   });
+
+  const {myAnimalsList} = useContext(AppContext);
 
   const [userLocation, setUserLocation] = useState<{
     lat: number;
@@ -127,11 +28,15 @@ export const Maps = () => {
   >([]);
   const [totalDistance, setTotalDistance] = useState<number>(0);
   const markerIdCounter = useRef(0);
+  const [selectedPetId, setSelectedPetId] = useState<number[]>([]); 
+
+  
+
 
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        (position) => {
+        (position: GeolocationPosition) => {
           setUserLocation({
             lat: position.coords.latitude,
             lng: position.coords.longitude,
@@ -141,7 +46,7 @@ export const Maps = () => {
             lng: position.coords.longitude,
           });
         },
-        (error) => {
+        (error: GeolocationPositionError) => {
           console.error("Error fetching user location:", error);
         }
       );
@@ -165,25 +70,28 @@ export const Maps = () => {
     setTotalDistance(distance);
   }, [markers]);
 
-  const calculateDistance = (lat1, lng1, lat2, lng2) => {
+  const calculateDistance = (lat1: number, lng1:number, lat2: number, lng2: number) => {
     const earthRadius = 6371; // Radius of the Earth in kilometers
-  
+
     // Convert latitude and longitude from degrees to radians
     const latRad1 = (lat1 * Math.PI) / 180;
     const lngRad1 = (lng1 * Math.PI) / 180;
     const latRad2 = (lat2 * Math.PI) / 180;
     const lngRad2 = (lng2 * Math.PI) / 180;
-  
+
     // Calculate the differences between latitudes and longitudes
     const latDiff = latRad2 - latRad1;
     const lngDiff = lngRad2 - lngRad1;
-  
+
     // Calculate the Haversine formula
     const a =
       Math.sin(latDiff / 2) * Math.sin(latDiff / 2) +
-      Math.cos(latRad1) * Math.cos(latRad2) * Math.sin(lngDiff / 2) * Math.sin(lngDiff / 2);
+      Math.cos(latRad1) *
+        Math.cos(latRad2) *
+        Math.sin(lngDiff / 2) *
+        Math.sin(lngDiff / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  
+
     // Calculate the distance
     const distance = earthRadius * c;
     return distance;
@@ -191,7 +99,7 @@ export const Maps = () => {
 
   useEffect(() => {
     let distance = 0;
-  
+
     for (let i = 1; i < markers.length; i++) {
       const prevMarker = markers[i - 1];
       const currMarker = markers[i];
@@ -201,12 +109,9 @@ export const Maps = () => {
         currMarker.lat,
         currMarker.lng
       );
-    }
-  
+      }
     setTotalDistance(distance);
-    console.log(distance)
   }, [markers]);
-  
 
   const handleMapClick = (event: google.maps.MapMouseEvent) => {
     if (event.latLng) {
@@ -261,7 +166,37 @@ export const Maps = () => {
               )}
               {totalDistance > 0 && (
                 <div className={classes.distance}>
-                  Total Distance: {totalDistance.toFixed(2)} km
+                  <h3>Your Walk</h3>
+                  <p>Total Distance: {totalDistance.toFixed(2)} km</p>
+                  
+                  <select
+  value={selectedPetId ?? ""}
+  onChange={(e) => {
+    const selectedId = parseInt(e.target.value);
+    setSelectedPetId([selectedId]); ;
+  }}
+>
+  <option value="">Select a pet</option>
+  {myAnimalsList.map((pet) => (
+    <option key={pet.id} value={pet.id}>
+      {pet.name}
+    </option>
+    
+  ))}
+</select>
+{selectedPetId.map((id: number) => {
+  const selectedPet = myAnimalsList.find(pet => pet.id === id);
+  if (selectedPet) {
+    return (
+      <div key={selectedPet.id}>
+        <p>Pet: {selectedPet.name}</p>
+      </div>
+    );
+  } else {
+    return null; // Handle the case where the selected pet is not found
+      }
+    })}
+    <button className={classes.button}>Go!</button>
                 </div>
               )}
             </>
